73c4af624fff8ab27fa8521f7efe6d1a0a2ab8d5
mohamed lellah <mohamed.gleigum@acpec-sarl.com>
2025-11-09 19:33:31 +0000

    Étape 4 – Création du contrôleur et des routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EtudiantController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('etudiants', EtudiantController::class);
