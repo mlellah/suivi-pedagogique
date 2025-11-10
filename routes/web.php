6d885ccff126e20373556b9fbd812fa1facc7ea7
mohamed lellah <mohamed.gleigum@acpec-sarl.com>
2025-11-10 09:04:23 +0000

    Étape 5 – Création de la vue Étudiants.vue

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,24 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EtudiantController;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-
-Route::resource('etudiants', EtudiantController::class);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
